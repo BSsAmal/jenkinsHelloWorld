5483488bd268b8ca20415599e0a5b06a8ae1d482
Amal BEN SASSI <79196316+BSsAmal@users.noreply.github.com>
2022-10-07 12:02:02 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 public static void main (String[] args){
-	 System.out.println("Hello World & Welcome to jenkins training") ;
+	 System.out.println("Hello World & Welcome to jenkins training, Im making some chnaging to see if it's working on jenkins it self ") ;
 }
 }
